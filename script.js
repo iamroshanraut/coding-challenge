67c611f7b8f3bf343f2e8cc41b0ce0c3e3697157
Roshan Raut <rautroshan601@gmail.com>
2023-11-09 22:57:18 +0530

    Did a Coding Challenge

---
+++ script.js
@@ -0,0 +1,23 @@
+// Coding Challenge
+
+const juliasDogs = [3, 5, 2, 12, 7];
+const kateDogs = [4, 1, 15, 8, 3];
+
+function checkDogs(dogsJulia, dogsKate) {
+  const juliasDogsCopy = dogsJulia.slice();
+  juliasDogsCopy.splice(0, 1);
+  juliasDogsCopy.splice(-2);
+
+  const allDogs = dogsJulia.concat(dogsKate);
+
+  allDogs.forEach(function (age, ind) {
+    age >= 3
+      ? console.log(
+          `Dog number ${ind + 1} is an adult, and is ${age} years old`
+        )
+      : console.log(`Dog number ${ind + 1} is still a puppy`);
+  });
+}
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
