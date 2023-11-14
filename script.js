f7cf075330218a9d951a1503c17241fb1ab883c8
Roshan Raut <rautroshan601@gmail.com>
2023-11-14 11:38:15 +0530

    Added variables as arguments

--- script.js
+++ script.js
@@ -19,5 +19,5 @@ function checkDogs(dogsJulia, dogsKate) {
   });
 }
 
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs(juliasDogs, kateDogs);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
